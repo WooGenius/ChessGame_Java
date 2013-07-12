383d2963caba24ed3c83361e796bba5e714c93af
Yoon <lvev9925@naver.com>
2013-07-12 15:39:37 +0900

    연습문제 1-4

--- test/PawnTest.java
+++ test/PawnTest.java
@@ -3,6 +3,8 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		new Pawn();
+		Pawn pawn1 = new Pawn();
+		String color = pawn1.getColor();
+		assertEquals("white", color);		
 	}
 }
