af730302dc0bd3985b416c559195a10bcddeff10
Yoon <lvev9925@naver.com>
2013-07-12 15:44:21 +0900

    연습문제 1-5

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,4 +1,18 @@
 
 public class Pawn {
 
+	String color;
+	
+	public Pawn(){
+		color = "white";
+	}
+	
+	public Pawn(String color) {
+		this.color = color; 
+	}
+
+	String getColor() {
+		return color;
+	}
+
 }
--- test/PawnTest.java
+++ test/PawnTest.java
@@ -4,7 +4,9 @@ import junit.framework.TestCase;
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
 		Pawn pawn1 = new Pawn();
-		String color = pawn1.getColor();
-		assertEquals("white", color);		
+		assertEquals("white", pawn1.getColor());
+		
+		Pawn pawn2 = new Pawn("black");
+		assertEquals("black", pawn2.getColor());
 	}
 }
