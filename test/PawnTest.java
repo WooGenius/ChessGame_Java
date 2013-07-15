3f14c533cb25e0b523fdb27639632d731723dde8
WooGenius <woogenius@nhnnext.org>
2013-07-15 23:43:56 +0900

    1-6_fixed

--- test/PawnTest.java
+++ test/PawnTest.java
@@ -3,11 +3,11 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		String white = "white";
-		String black = "black";
+		String a = "white";
+		String b = "black";
 		Pawn pawn1 = new Pawn();
-		assertEquals(white, pawn1.getColor());
-		Pawn pawn2 = new Pawn(black);
-		assertEquals(black, pawn2.getColor());
+		assertEquals(a, pawn1.getColor());
+		Pawn pawn2 = new Pawn(b);
+		assertEquals(b, pawn2.getColor());
 	}
 }
