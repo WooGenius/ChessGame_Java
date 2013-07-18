02c65a9fa37aa781ef7db13e5570e95a74ee8883
WooGenius <woogenius@nhnnext.org>
2013-07-19 03:27:52 +0900

    lesson3_fixed 대문자 소문자 중복 개

---
+++ test/chess/RowsTest.java
@@ -0,0 +1,13 @@
+package chess;
+
+import java.util.ArrayList;
+
+import pieces.Pawn;
+import junit.framework.TestCase;
+
+public class RowsTest extends TestCase {
+	public void testCreate() throws Exception {
+		Rows row = new Rows();
+		
+	}
+}
