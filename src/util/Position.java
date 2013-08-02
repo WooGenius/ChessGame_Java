4e8207288eb04d8d98687f26f2760e8c644d5fe3
WooGenius <woogenius@nhnnext.org>
2013-08-02 17:18:45 +0900

    5-6_5 position 메소드를 다시 인스턴스 메소드로 변경

--- src/util/Position.java
+++ src/util/Position.java
@@ -1,18 +1,15 @@
 package util;
 
 public class Position {
+	private int posX;
+	private int posY;
 	
-	public static int getX(String pos) {
-		int posX = getIntPos(pos.charAt(0));
-		return posX;
+	public Position(String position) {
+		posX = getIntPos(position.charAt(0));
+		posY = getIntPos(position.charAt(1));
 	}
-	
-	public static int getY(String pos) {
-		int posY = getIntPos(pos.charAt(1));
-		return posY;
- 	}
-	
-	private static int getIntPos(char pos) {	// char를 int로 바꾸는 메소드
+
+	private int getIntPos(char pos) {	// char를 int로 바꾸는 메소드
 		final int CONSTANT_X = 10;	// getNumericValue와 체스판 포지션의 차이를 보정해주는 상수
 		final int CONSTANT_Y = 1;
 		if (Character.isDigit(pos)) {
@@ -21,4 +18,11 @@ public class Position {
 		return Character.getNumericValue(pos) - CONSTANT_X;
 	}
 
+	public int getX() {
+		return posX;
+	}
+
+	public int getY() {
+		return posY;
+	}
 }
--- test/util/PositionTest.java
+++ test/util/PositionTest.java
@@ -4,9 +4,11 @@ import junit.framework.TestCase;
 
 public class PositionTest extends TestCase {
 	public void testCreate() throws Exception {
-		assertEquals(2, Position.getX("c8"));
-		assertEquals(7, Position.getY("c8"));
-		assertEquals(4, Position.getX("e6"));
-		assertEquals(5, Position.getY("e6"));
+		Position position = new Position("c8");
+		assertEquals(2, position.getX());
+		assertEquals(7, position.getY());
+		position = new Position("e6");
+		assertEquals(4, position.getX());
+		assertEquals(5, position.getY());
 	}
 }
